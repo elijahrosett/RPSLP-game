7987d3bdeec6c09b9a1ec8d48c1bb78d5e489507
Robert Gessler <ragessler@roberts-mbp.lan>
2022-04-06 15:19:20 -0500

    cleaned up some whitespace

--- arena.py
+++ arena.py
@@ -1,30 +1,17 @@
 from human import Human
 from ai import Ai
-
-
-
-
-
-
 class Arena:
     def __init__(self):
         self.player_one = None
         self.player_two = None
-        
-
     def run_game(self):
         self.greeting()
         self.how_many_players()
         while self.player_one.score < 2 and self.player_two.score < 2:
             self.play_round()
         self.display_winner()
-
-
-
     def greeting(self):
         print("Welcome to rock, paper, scissors, Spock, lizard!")
-        
-
     def how_many_players(self):
         user_input = input("How many people will be playing?")
         if user_input == '1':
@@ -38,18 +25,17 @@ class Arena:
         else:
             print('Invalid Input please type 1 or 2')
             self.how_many_players()
-    
     def play_round(self):
         self.player_one.gesture_pick()
         self.player_two.gesture_pick()
         self.gesture_decider()
-    
+        print(f'{self.player_one.name} has a score of {self.player_one.score}')
+        print(f'{self.player_two.name} has a score of {self.player_two.score}')
     def display_winner(self):
         if self.player_one.score >= 2:
             print(f'{self.player_one.name} Wins!')
         elif self.player_two.score >= 2:
             print(f'{self.player_two.name} Wins!')
-
     def gesture_decider(self):
         if self.player_one.gesture == 'rock' and self.player_two.gesture == 'scissors':
             self.player_one.score += 1
@@ -114,7 +100,3 @@ class Arena:
         else:
             print(f'{self.player_one.gesture} ties with {self.player_two.gesture}')
             print(f'Both players used {self.player_one.gesture} and tied!')
-#greeting
-#how many players?
-#if 1 player_two = human
-#elif 2 player_two = ai
